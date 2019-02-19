c116d2da3601f84558a8b2d84ebd221bed0e5f9a
BulatBubeev <bula3@live.ru>
2019-02-19 15:34:02 +0800

    Email setting for sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -13,12 +13,3 @@ end
 # Initialize the Rails application
 Rails.application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'redminesd.herokuapp.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
